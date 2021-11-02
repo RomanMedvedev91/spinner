0b2637eeec6b2b78c9917ad233f59587a20fb114
RomanMedvedev91 <roman.medvedev77@gmail.com>
2021-11-02 22:06:49 +0000

    finish spinner

---
+++ spinner1.js
@@ -0,0 +1,26 @@
+process.stdout.write("hello from spinner1.js... \rheyyy\n");
+
+setTimeout(() => {
+  process.stdout.write("\r|   ");
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write("\r/   ");
+}, 400);
+
+setTimeout(() => {
+  process.stdout.write("\r-   ");
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write("\r|   ");
+}, 1000);
+setTimeout(() => {
+  process.stdout.write("\r/   ");
+}, 1300);
+setTimeout(() => {
+  process.stdout.write("\r-   ");
+}, 1600);
+setTimeout(() => {
+  process.stdout.write("\r|\n");
+}, 1900);
